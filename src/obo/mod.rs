c77ce45d302e8330726840535f3d827504656b6f
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-10-09 15:57:26 +0200

    Add links to OBO guides to `obo` documentation

--- src/obo/mod.rs
+++ src/obo/mod.rs
@@ -1,3 +1,8 @@
 //! OBO Flat File Format syntax tree.
+//!
+//! # See Also
+//!
+//! - [The OBO Flat File Format Guide, version 1.4](https://owlcollab.github.io/oboformat/doc/GO.format.obo-1_4.html#S.2.2.3)
+//! - [The OBO Flat File Format Syntax and Semantics, version 1.4](http://purl.obolibrary.org/obo/oboformat/spec.html)
 
 pub mod ast;

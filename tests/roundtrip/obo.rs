cdae55526f26b53c643d3f6749b07a7e378cea7c
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-10-16 16:53:15 +0200

    Add integration tests based on header roundtripping for OBO

---
+++ tests/roundtrip/obo.rs
@@ -0,0 +1,26 @@
+macro_rules! test_roundtrip_header {
+    ($name:ident, $path:tt) => {
+        #[test]
+        fn $name() {
+            use std::str::FromStr;
+            use std::string::ToString;
+            use $crate::ontology::obo::ast::HeaderFrame;
+
+            let data = include_str!($path);
+            let i = data.find("\n\n").unwrap();
+            let header = &data[..i + 1];
+
+            println!("{}", header);
+
+            let tree = HeaderFrame::from_str(header).expect("parser failed");
+            let out = tree.to_string();
+
+            assert_eq!(out, header)
+        }
+    };
+}
+
+pub mod header {
+    test_roundtrip_header!(psi_ms, "../data/psi-ms.obo");
+    test_roundtrip_header!(ro, "../data/ro.obo");
+}

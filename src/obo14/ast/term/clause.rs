a7ee360e9e79f8ab8da715f133f0d8102e02533b
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-19 02:31:14 +0100

    Add `FromPair` implementation to `obo14::ast::TermClause` and `obo14::ast::TermFrame`

--- src/obo14/ast/term/clause.rs
+++ src/obo14/ast/term/clause.rs
@@ -2,7 +2,13 @@ use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Result as FmtResult;
 use std::fmt::Write;
+use std::str::FromStr;
 
+use pest::iterators::Pair;
+
+use crate::obo14::parser::FromPair;
+use crate::obo14::parser::Parser;
+use crate::obo14::parser::Rule;
 use crate::obo14::ast::ClassId;
 use crate::obo14::ast::Id;
 use crate::obo14::ast::IsoDate;
@@ -18,6 +24,7 @@ use crate::obo14::ast::SynonymTypeId;
 use crate::obo14::ast::UnquotedString;
 use crate::obo14::ast::Xref;
 use crate::obo14::ast::XrefList;
+use crate::error::Result;
 
 /// A clause appearing in a term frame.
 #[derive(Debug, Eq, Hash, PartialEq)]
@@ -50,7 +57,6 @@ pub enum TermClause {
     // IsMetadata(bool),
     // IsClassLevel(bool),
 }
-
 
 impl Display for TermClause {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
@@ -94,4 +100,165 @@ impl Display for TermClause {
             CreationDate(date) => f.write_str("creation_date: ").and(date.fmt(f)),
         }
     }
+}
+
+impl FromPair for Line<TermClause> {
+    const RULE: Rule = Rule::TermClauseLine;
+    unsafe fn from_pair_unchecked(pair: Pair<Rule>) -> Result<Self> {
+        let mut inner = pair.into_inner();
+        let clause = TermClause::from_pair_unchecked(inner.next().unwrap())?;
+        let eol = inner.next().unwrap();
+        Ok(Line::<()>::from_pair_unchecked(eol)?.with_content(clause))
+    }
+}
+impl_fromstr!(Line<TermClause>);
+
+impl FromPair for TermClause {
+    const RULE: Rule = Rule::TermClause;
+    unsafe fn from_pair_unchecked(pair: Pair<Rule>) -> Result<Self> {
+        let mut inner = pair.into_inner();
+        match inner.next().unwrap().as_rule() {
+            Rule::IsAnonymousTag => {
+                let b = bool::from_str(inner.next().unwrap().as_str()).unwrap();
+                Ok(TermClause::IsAnonymous(b))
+            }
+            Rule::NameTag => {
+                let name = UnquotedString::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Name(name))
+            }
+            Rule::NamespaceTag => {
+                let ns = NamespaceId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Namespace(ns))
+            }
+            Rule::AltIdTag => {
+                let id = Id::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::AltId(id))
+            }
+            Rule::DefTag => {
+                let def = QuotedString::from_pair_unchecked(inner.next().unwrap())?;
+                let xrefs = XrefList::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Def(def, xrefs))
+            }
+            Rule::CommentTag => {
+                let comment = UnquotedString::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Comment(comment))
+            }
+            Rule::SubsetTag => {
+                let id = SubsetId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Subset(id))
+            }
+            Rule::SynonymTag => {
+                let desc = QuotedString::from_pair_unchecked(inner.next().unwrap())?;
+                let scope = SynonymScope::from_pair_unchecked(inner.next().unwrap())?;
+
+                let pair = inner.next().unwrap();
+                match pair.as_rule() {
+                    Rule::SynonymTypeId => {
+                        let ty = SynonymTypeId::from_pair_unchecked(pair)?;
+                        let xrefs = XrefList::from_pair_unchecked(inner.next().unwrap())?;
+                        Ok(TermClause::Synonym(desc, scope, Some(ty), xrefs))
+                    }
+                    Rule::XrefList => {
+                        let xrefs = XrefList::from_pair_unchecked(pair)?;
+                        Ok(TermClause::Synonym(desc, scope, None, xrefs))
+                    }
+                    _ => unreachable!()
+                }
+            }
+            Rule::XrefTag => {
+                let xref = Xref::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Xref(xref))
+            }
+            Rule::BuiltinTag => {
+                let b = bool::from_str(inner.next().unwrap().as_str()).unwrap();
+                Ok(TermClause::Builtin(b))
+            }
+            Rule::PropertyValueTag => {
+                let pv = PropertyValue::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::PropertyValue(pv))
+            }
+            Rule::IsATag => {
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::IsA(id))
+            }
+            Rule::IntersectionOfTag => {
+                let id = inner.next().unwrap();
+                if id.as_rule() == Rule::ClassId {
+                    let classid = ClassId::from_pair_unchecked(id)?;
+                    Ok(TermClause::IntersectionOf(None, classid))
+                } else {
+                    let relid = RelationId::from_pair_unchecked(id)?;
+                    let classid = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                    Ok(TermClause::IntersectionOf(Some(relid), classid))
+                }
+            }
+            Rule::UnionOfTag => {
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::UnionOf(id))
+            }
+            Rule::EquivalentToTag => {
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::EquivalentTo(id))
+            }
+            Rule::DisjointFromTag => {
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::DisjointFrom(id))
+            }
+            Rule::RelationshipTag => {
+                let rel = RelationId::from_pair_unchecked(inner.next().unwrap())?;
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Relationship(rel, id))
+            }
+            Rule::IsObsoleteTag => {
+                let b = bool::from_str(inner.next().unwrap().as_str()).unwrap();
+                Ok(TermClause::IsObsolete(b))
+            }
+            Rule::ReplacedByTag => {
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::ReplacedBy(id))
+            }
+            Rule::ConsiderTag => {
+                let id = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::Consider(id))
+            }
+            Rule::CreatedByTag => {
+                let id = PersonId::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::CreatedBy(id))
+            }
+            Rule::CreationDateTag => {
+                let dt = IsoDate::from_pair_unchecked(inner.next().unwrap())?;
+                Ok(TermClause::CreationDate(dt))
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+impl_fromstr!(TermClause);
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::str::FromStr;
+
+    use crate::obo14::ast::PrefixedId;
+    use crate::obo14::ast::IdPrefix;
+    use crate::obo14::ast::IdLocal;
+    use crate::obo14::ast::Xref;
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        let actual = TermClause::from_str("name: sample name").unwrap();
+        let expected = TermClause::Name(UnquotedString::new("sample name"));
+        assert_eq!(actual, expected);
+
+        let actual = TermClause::from_str("def: \"A reference string relevant to the sample under study.\" [PSI:MS]").unwrap();
+        let expected = TermClause::Def(
+            QuotedString::new("A reference string relevant to the sample under study."),
+            XrefList::from(vec![Xref::new(Id::Prefixed(PrefixedId::new(IdPrefix::new("PSI"), IdLocal::new("MS"))))])
+        );
+        assert_eq!(actual, expected)
+    }
+
 }
--- src/obo14/ast/term/frame.rs
+++ src/obo14/ast/term/frame.rs
@@ -3,9 +3,15 @@ use std::fmt::Formatter;
 use std::fmt::Result as FmtResult;
 use std::fmt::Write;
 
+use pest::iterators::Pair;
+
+use crate::obo14::parser::FromPair;
+use crate::obo14::parser::Parser;
+use crate::obo14::parser::Rule;
 use crate::obo14::ast::Line;
 use crate::obo14::ast::ClassId;
 use super::TermClause;
+use crate::error::Result;
 
 /// A term frame, describing a class.
 pub struct TermFrame {
@@ -17,6 +23,52 @@ impl Display for TermFrame {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         f.write_str("[Term]\nid: ").and(self.id.fmt(f))?;
         self.clauses.iter().try_for_each(|clause| clause.fmt(f))
+    }
+}
+
+impl FromPair for TermFrame {
+    const RULE: Rule = Rule::TermFrame;
+    unsafe fn from_pair_unchecked(pair: Pair<Rule>) -> Result<Self> {
+        let mut inner = pair.into_inner();
+        let clsid = ClassId::from_pair_unchecked(inner.next().unwrap())?;
+        let id = Line::<()>::from_pair_unchecked(inner.next().unwrap())?
+            .with_content(clsid);
+
+        let mut clauses = Vec::new();
+        for pair in inner {
+            clauses.push(Line::<TermClause>::from_pair_unchecked(pair)?);
+        }
+
+        Ok(TermFrame { id, clauses })
+    }
+}
+impl_fromstr!(TermFrame);
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::str::FromStr;
+
+    use crate::obo14::ast::PrefixedId;
+    use crate::obo14::ast::IdPrefix;
+    use crate::obo14::ast::IdLocal;
+    use crate::obo14::ast::Id;
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        let actual = TermFrame::from_str(
+            "[Term]
+            id: MS:1000008
+            name: ionization type
+            def: \"The method by which gas phase ions are generated from the sample.\" [PSI:MS]
+            relationship: part_of MS:1000458 ! source\n"
+        ).unwrap();
+        assert_eq!(
+            actual.id.as_ref(),
+            &ClassId::from(Id::from(PrefixedId::new(IdPrefix::new("MS"), IdLocal::new("1000008"))))
+        );
     }
 
 }

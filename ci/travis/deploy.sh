da8aabe045fdd8e2299acad7ff1ad8a932f2edff
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-31 03:58:51 +0200

    Improve Travis-CI deployment script to select crate from tag

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -9,10 +9,34 @@ log() {
 		echo $@
 }
 
+error() {
+		tput bold
+		tput setaf 1
+		printf "%12s " $1
+		tput sgr0
+		shift 1
+		echo $@
+}
+
+
 
 # --- Publish crate to `crates.io` ---------------------------------------------
 
-cargo publish --token $CRATES_IO_TOKEN
+case "$TRAVIS_TAG" in
+	v*)
+			log Publishing fastobo $TRAVIS_TAG
+			cargo publish --token $CRATES_IO_TOKEN --dry-run
+			;;
+	syntax/v*)
+			cd fastobo-syntax
+			log Publishing fastobo-syntax ${TRAVIS_TAG#syntax/}
+			cargo publish --token $CRATES_IO_TOKEN --dry-run
+			;;
+	*)
+			error Error invalid or missing tag: $TRAVIS_TAG
+			exit 1
+			;;
+esac
 
 
 # --- Update release tags using Chandler ---------------------------------------
@@ -20,8 +44,8 @@ cargo publish --token $CRATES_IO_TOKEN
 export GEM_PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')"
 export PATH="${GEM_PATH}/bin:$PATH"
 
-log "  Installing" "chandler gem"
+log Installing chandler gem
 gem install --user-install chandler
 
-log "    Updating" "GitHub release notes"
+log Updating GitHub release notes
 chandler push --github="$TRAVIS_REPO_SLUG"

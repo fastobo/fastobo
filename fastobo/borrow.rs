e66664f4aee9fe0407a9f7acd878df19f332f4f0
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-04-03 00:14:09 +0200

    Add more blanket implementations to `fastobo::borrow`

--- fastobo/borrow.rs
+++ fastobo/borrow.rs
@@ -4,16 +4,48 @@
 //! - `Borrow::borrow` does not need to return a reference.
 //! - `Borrow::borrow` has access to the lifetime of the referenced owner.
 //! - `impl ToOwned<Owned=T> for B for` does not require `impl Borrow<B> for T`.
-//! - `Cow` needs explicit references (e.g. `Cow<'a, &'a str>`).
+//! - `Cow` needs explicit references where (e.g. `Cow<'a, &'a str>`).
 //!
 //! This allow a `Cow`-like behaviour on enum structures referencing pointers.
+//!
+//! # Examples
+//!
+//! ```rust
+//! # extern crate fastobo;
+//! use fastobo::borrow::{Borrow, Cow, ToOwned};
+//!
+//! pub struct MyOwner(String);
+//!
+//! #[derive(Clone)]
+//! pub struct MyRef<'a>(Cow<'a, &'a str>);
+//!
+//! impl<'a> Borrow<'a, MyRef<'a>> for MyOwner {
+//!     fn borrow(&'a self) -> MyRef<'a> {
+//!         MyRef(Cow::from(self.0.as_ref()))
+//!     }
+//! }
+//!
+//! impl<'a> ToOwned<'a> for MyRef<'a> {
+//!     type Owned = MyOwner;
+//!     fn to_owned(&'a self) -> Self::Owned {
+//!         MyOwner(self.0.to_string())
+//!     }
+//! }
+//! ```
+
 
 use std::fmt::Display;
+use std::fmt::Debug;
 use std::fmt::Formatter;
 use std::fmt::Result as FmtResult;
 use std::fmt::Write;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::ops::Deref;
 
 use self::Cow::*;
+
+// --- Borrow ----------------------------------------------------------------
 
 pub trait Borrow<'a, Borrowed: 'a> {
     fn borrow(&'a self) -> Borrowed;
@@ -27,6 +59,14 @@ where
         <Self as std::borrow::Borrow<B>>::borrow(self)
     }
 }
+
+impl<'a> Borrow<'a, &'a str> for String {
+    fn borrow(&'a self) -> &'a str {
+        &self.as_ref()
+    }
+}
+
+// --- ToOwned ---------------------------------------------------------------
 
 pub trait ToOwned<'a>
 where
@@ -48,6 +88,15 @@ where
         <Self as std::borrow::ToOwned>::to_owned(self)
     }
 }
+
+impl<'a> ToOwned<'a> for &'a str {
+    type Owned = String;
+    fn to_owned(&'a self) -> String {
+        self.to_string()
+    }
+}
+
+// --- Cow -------------------------------------------------------------------
 
 pub enum Cow<'a, B: 'a>
 where
@@ -91,58 +140,9 @@ where
     &'a B: ToOwned<'a>
 {
     fn from(b: &'a B) -> Self {
-        Cow::Borrowed(b)
+        Borrowed(b)
     }
 }
-
-// impl<'a, B> Cow<'a, B>
-// where
-//     B: ToOwned<'a>,
-//     <B as ToOwned<'a>>::Owned: 'a,
-// {
-//     /// Extracts the owned data.
-//     ///
-//     /// Clones the data if it is not already owned.
-//     ///
-//     /// # Examples
-//     ///
-//     /// Calling `into_owned` on a `Cow::Borrowed` clones the underlying data
-//     /// and becomes a `Cow::Owned`:
-//     ///
-//     /// ```
-//     /// use std::borrow::Cow;
-//     ///
-//     /// let s = "Hello world!";
-//     /// let cow = Cow::Borrowed(s);
-//     ///
-//     /// assert_eq!(
-//     ///   cow.into_owned(),
-//     ///   String::from(s)
-//     /// );
-//     /// ```
-//     ///
-//     /// Calling `into_owned` on a `Cow::Owned` is a no-op:
-//     ///
-//     /// ```
-//     /// use std::borrow::Cow;
-//     ///
-//     /// let s = "Hello world!";
-//     /// let cow: Cow<str> = Cow::Owned(String::from(s));
-//     ///
-//     /// assert_eq!(
-//     ///   cow.into_owned(),
-//     ///   String::from(s)
-//     /// );
-//     /// ```
-//     pub fn into_owned(self) -> <B as ToOwned<'a>>::Owned {
-//         match self {
-//             Borrowed(borrowed) => {
-//                 *(&borrowed.to_owned() as *const _)
-//             },
-//             Owned(owned) => owned,
-//         }
-//     }
-// }
 
 impl<'a, B> Display for Cow<'a, B>
 where
@@ -150,12 +150,91 @@ where
     <B as ToOwned<'a>>::Owned: Display
 {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        match *self {
-            Borrowed(ref b) => b.fmt(f),
-            Owned(ref o) => o.fmt(f),
+        match self {
+            Borrowed(b) => b.fmt(f),
+            Owned(o) => o.fmt(f),
         }
     }
 }
+
+impl<'a, B> Debug for Cow<'a, B>
+where
+    B: Debug + ToOwned<'a>,
+    <B as ToOwned<'a>>::Owned: Debug,
+{
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            Borrowed(b) => b.fmt(f),
+            Owned(o) => o.fmt(f),
+        }
+    }
+}
+
+impl<'a, B> Hash for Cow<'a, B>
+where
+    B: Hash + ToOwned<'a>,
+    <B as ToOwned<'a>>::Owned: Hash,
+{
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher
+    {
+        match self {
+            Borrowed(b) => b.hash(state),
+            Owned(o) => o.hash(state),
+        }
+    }
+}
+
+// --- Cow<str> --------------------------------------------------------------
+
+impl<'a> Cow<'a, &'a str> {
+    fn as_str(&self) -> &str {
+        match self {
+            Borrowed(s) => *s,
+            Owned(s) => s.as_str(),
+        }
+    }
+}
+
+impl<'a> Deref for Cow<'a, &'a str> {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Borrowed(s) => s.clone(),
+            Owned(ref s) => s.as_ref(),
+        }
+    }
+}
+
+impl<'a> Eq for Cow<'a, &'a str> {}
+
+impl<'a> From<&'a str> for Cow<'a, &'a str> {
+    fn from(s: &'a str) -> Self {
+        Borrowed(s)
+    }
+}
+
+impl<'a> From<String> for Cow<'a, &'a str> {
+    fn from(s: String) -> Self {
+        Owned(s)
+    }
+}
+
+impl<'a> PartialEq<str> for Cow<'a, &'a str> {
+    fn eq(&self, other: &str) -> bool {
+        self.as_str() == other
+    }
+}
+
+impl<'a> PartialEq for Cow<'a, &'a str> {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_str() == other.as_str()
+    }
+}
+
+
+// --- Cow<[T]>
 
 impl<'a, T> ToOwned<'a> for &'a [T]
 where

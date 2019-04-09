2f951e7d7d394925ed7d45245be8516431a69aa7
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-04-10 00:40:33 +0200

    Reorganize project into a single workspace

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools_rust as rust
 setuptools.setup(
     rust_extensions=[rust.RustExtension(
         "fastobo",
-        path="Cargo.toml",
+        path="fastobo-py/Cargo.toml",
         binding=rust.Binding.PyO3,
         strip=rust.Strip.Debug
     )],

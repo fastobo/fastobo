297e51e6ef0ae66bf023dfc4f16970de8861e6d3
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-04-11 20:09:22 +0200

    Add `Clone` to `fastobo-py::pyfile::PyFile`

--- fastobo-py/src/pyfile.rs
+++ fastobo-py/src/pyfile.rs
@@ -7,8 +7,9 @@ use pyo3::AsPyPointer;
 use pyo3::PyDowncastError;
 use pyo3::types::PyBytes;
 
+#[derive(Clone, Debug)]
 pub struct PyFile<'p> {
-    file: PyObject,
+    file: *mut pyo3::ffi::PyObject,
     __data: PhantomData<&'p PyObject>
 }
 
@@ -22,7 +23,7 @@ impl<'p> PyFile<'p> {
             if let Ok(res) = file.call_method1(py, "read", (0, )) {
                 if py.is_instance::<PyBytes, PyObject>(&res).unwrap_or(false) {
                     Ok(PyFile {
-                        file,
+                        file: obj.as_ptr(),
                         __data: PhantomData
                     })
                 } else {
@@ -37,14 +38,18 @@ impl<'p> PyFile<'p> {
 
 impl<'p> Read for PyFile<'p> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let py = unsafe { Python::assume_gil_acquired() };
-        let res = self.file.call_method1(py, "read", (buf.len(), ))
-            .unwrap(); // FIXME -> map as OS Error
-        let bytes = res
-            .extract::<&PyBytes>(py)
-            .unwrap();
-        let b = bytes.as_bytes();
-        (&mut buf[..b.len()]).copy_from_slice(b);
-        Ok(b.len())
+        unsafe {
+            let py = Python::assume_gil_acquired();
+            let file = PyObject::from_borrowed_ptr(py, self.file);
+            let res = file.call_method1(py, "read", (buf.len(), ))
+                .unwrap(); // FIXME -> map as OS Error
+            let bytes = res
+                .extract::<&PyBytes>(py)
+                .unwrap();
+            let b = bytes.as_bytes();
+            (&mut buf[..b.len()]).copy_from_slice(b);
+            Ok(b.len())
+        }
+
     }
 }

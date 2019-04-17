2d277b43f1b2f428bbf14765351f4dfc18899a62
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-04-17 23:58:35 +0200

    Fix `sccache` missing from OSX builds in Travis-CI

--- ci/travis/osx/install.sh
+++ ci/travis/osx/install.sh
@@ -23,3 +23,20 @@ curl -sSf https://build.travis-ci.org/files/rustup-init.sh | sh -s -- --default-
 
 log Installing Python requirements
 $PYTHON -m pip install -r "$TRAVIS_BUILD_DIR/ci/requirements.txt"
+
+
+# --- Setup sccache ----------------------------------------------------------
+
+LATEST=$(cargo search sccache | grep sccache | cut -f2 -d"\"")
+LOCAL=$(sccache --version 2>/dev/null | cut -f2 -d" " || echo "none")
+
+if [ "$LATEST" != "$LOCAL" ]; then
+	log Downloading sccache v$LATEST
+  URL="https://github.com/mozilla/sccache/releases/download/${LATEST}/sccache-${LATEST}-x86_64-apple-darwin.tar.gz"
+	curl -SsL $URL | tar xzv -C /tmp
+	mkdir -p "$HOME/.cargo/bin"
+	mv "/tmp/sccache-${LATEST}-x86_64-apple-darwin/sccache" "$HOME/.cargo/bin/sccache"
+	chmod +x "$HOME/.cargo/bin/sccache"
+else
+	log Using cached sccache v$LOCAL
+fi

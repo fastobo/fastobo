0787d7a395d22a78302920b4f3a0ef977174ccd9
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-04-18 03:13:40 +0200

    Add `parse` method to `OboParser` to avoid needing `pest` in scope to parse

--- fastobo-syntax/src/lib.rs
+++ fastobo-syntax/src/lib.rs
@@ -2,7 +2,16 @@
 extern crate pest_derive;
 extern crate pest;
 
+use pest::error::Error;
+use pest::iterators::Pairs;
+
 /// The OBO format version 1.4 parser.
 #[derive(Debug, Parser)]
 #[grammar = "grammar.pest"]
 pub struct OboParser;
+
+impl OboParser {
+    pub fn parse(rule: Rule, input: &str) -> Result<Pairs<Rule>, Error<Rule>> {
+        <Self as pest::Parser<Rule>>::parse(rule, input)
+    }
+}

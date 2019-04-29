f733a1146e348bfa7d0bb00edd941bfb6958dc6a
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-04-29 20:09:37 +0200

    Separate `fastobo::ext` module into several files

---
+++ fastobo/src/ext/mod.rs
@@ -0,0 +1,216 @@
+//! A selection of useful traits that exceed the scope of OBO syntax.
+
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use crate::ast::*;
+use crate::error::CardinalityError;
+use crate::visit::VisitMut;
+
+mod treat_xrefs;
+
+// TODO: implement for
+// - EntityFrame
+// - TermFrame
+// - TypedefFrame
+// - InstanceFrame
+// - HeaderClause
+// - TermClause
+// - TypedefClause
+// - EntityClause
+/// A trait for structs that can be serialized in a particular order.
+pub trait SerializationOrd {
+    fn serialization_cmp(&self, other: &Self) -> Ordering;
+}
+
+/// Additional methods for `OboDoc` that can be used to edit the syntax tree.
+///
+/// The OBO 1.4 semantics are used to process header macros or to add the
+/// default OBO namespace to all the frames of the document.
+pub trait OboSemantics {
+
+    /// Add implicit OBO clauses to the `OboDoc` syntax tree.
+    ///
+    /// The OBO standard defines some implicit clauses that are automatically
+    /// added to all documents, which can be added by this method. The
+    /// concerned header clauses are:
+    /// - `idspace: RO http://purl.obolibrary.org/obo/RO_`
+    /// - `idspace: BFO http://purl.obolibrary.org/obo/BFO_`
+    /// - `treat-xrefs-as-equivalent: RO`
+    /// - `treat-xrefs-as-equivalent: BFO`
+    ///
+    /// # Note
+    /// After having called this method, the syntax tree will behave as if
+    /// these clauses were in the original document. In particular, they will
+    /// be written down in case the syntax tree is to be serialized.
+    fn preprocess(&mut self);
+
+    /// Assign the ontology default namespace to all frames without one.
+    ///
+    /// This function will not check the cardinality of `namespace` clauses in
+    /// entity frames: it will only add a single `namespace` clause to all
+    /// frames that have none.
+    ///
+    /// # Errors
+    /// - `CardinalityError::MissingClause`: if the header frame does not
+    ///   contain any default namespace definition.
+    /// - `CardinalityError::DuplicateClauses` if the header frame does
+    ///   contain more than one default namespace definition.
+    ///
+    /// # Example
+    /// ```rust
+    /// # extern crate fastobo;
+    /// # use pretty_assertions::assert_eq;
+    /// # use std::str::FromStr;
+    /// # use std::string::ToString;
+    /// # use fastobo::ast::*;
+    /// # use fastobo::ext::OboSemantics;
+    /// let mut doc = OboDoc::from_str(
+    /// "default-namespace: TST
+    ///
+    /// [Term]
+    /// id: TST:01
+    ///
+    /// [Term]
+    /// id: PATO:0000001
+    /// namespace: quality
+    /// ").unwrap();
+    ///
+    /// doc.assign_namespaces().unwrap();
+    ///
+    /// assert_eq!(doc.to_string(),
+    /// "default-namespace: TST
+    ///
+    /// [Term]
+    /// id: TST:01
+    /// namespace: TST
+    ///
+    /// [Term]
+    /// id: PATO:0000001
+    /// namespace: quality
+    /// ");
+    ///
+    fn assign_namespaces(&mut self) -> Result<(), CardinalityError>;
+
+    /// Process macros in the header frame, adding clauses to relevant entities.
+    ///
+    /// Header macros are used to expand an ontology by overloading the
+    /// actual semantics of  `xref` clauses contained in several entity frames.
+    /// In case the translated clauses are already present in the document,
+    /// they *won't* be added a second time.
+    ///
+    /// # Note
+    /// After processing the document, none of the original `xrefs` will
+    /// be removed from the AST.
+    ///
+    /// # See also
+    /// - [Header Macro Translation](http://owlcollab.github.io/oboformat/doc/obo-syntax.html#4.4.2)
+    ///   section of the syntax and semantics guide.
+    fn treat_xrefs(&mut self);
+}
+
+
+impl OboSemantics for OboDoc {
+
+    fn preprocess(&mut self) {
+
+        let bfo_idspace = HeaderClause::Idspace(
+            IdentPrefix::new("BFO"),
+            Url::parse("http://purl.obolibrary.org/obo/BFO_").unwrap(),
+            None
+        );
+        if !self.header.contains(&bfo_idspace) {
+            self.header.push(bfo_idspace);
+        }
+
+        let ro_idspace = HeaderClause::Idspace(
+            IdentPrefix::new("RO"),
+            Url::parse("http://purl.obolibrary.org/obo/RO_").unwrap(),
+            None
+        );
+        if !self.header.contains(&ro_idspace) {
+            self.header.push(ro_idspace);
+        }
+
+        let bfo_macro = HeaderClause::TreatXrefsAsEquivalent(IdentPrefix::new("BFO"));
+        if !self.header.contains(&bfo_macro) {
+            self.header.push(bfo_macro);
+        }
+
+        let ro_macro =  HeaderClause::TreatXrefsAsEquivalent(IdentPrefix::new("RO"));
+        if !self.header.contains(&ro_macro) {
+            self.header.push(ro_macro);
+        }
+    }
+
+    fn assign_namespaces(&mut self) -> Result<(), CardinalityError>{
+
+        macro_rules! expand {
+            ($frame:ident, $clause:ident, $ns:ident, $outer:lifetime) => ({
+                for clause in $frame.iter() {
+                    if let $clause::Namespace(_) = clause.as_ref() {
+                        continue $outer
+                    }
+                }
+                $frame.push(Line::from($clause::Namespace($ns.clone())));
+            });
+        }
+
+        use self::EntityFrame::*;
+        let ns = self.header.default_namespace()?;
+        'outer: for entity in &mut self.entities {
+            match entity {
+                Term(x) => expand!(x, TermClause, ns, 'outer),
+                Typedef(x) => expand!(x, TypedefClause, ns, 'outer),
+                Instance(x) => expand!(x, InstanceClause, ns, 'outer),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn treat_xrefs(&mut self) {
+        use self::HeaderClause::*;
+        for clause in self.header.iter_mut() {
+            match clause {
+                TreatXrefsAsEquivalent(prefix) =>
+                    self::treat_xrefs::as_equivalent(
+                        &mut self.entities,
+                        &prefix
+                    ),
+                TreatXrefsAsIsA(prefix) =>
+                    self::treat_xrefs::as_is_a(
+                        &mut self.entities,
+                        &prefix
+                    ),
+                TreatXrefsAsHasSubclass(prefix) =>
+                    self::treat_xrefs::as_has_subclass(
+                        &mut self.entities,
+                        &prefix
+                    ),
+                TreatXrefsAsGenusDifferentia(prefix, rel, cls) =>
+                    self::treat_xrefs::as_genus_differentia(
+                        &mut self.entities,
+                        &prefix,
+                        &rel,
+                        &cls
+                    ),
+                TreatXrefsAsReverseGenusDifferentia(prefix, rel, cls) =>
+                    self::treat_xrefs::as_reverse_genus_differentia(
+                        &mut self.entities,
+                        &prefix,
+                        &rel,
+                        &cls
+                    ),
+                TreatXrefsAsRelationship(prefix, rel) =>
+                    self::treat_xrefs::as_relationship(
+                        &mut self.entities,
+                        &prefix,
+                        &rel,
+                    ),
+                _ => (),
+            }
+        }
+    }
+}
---
+++ fastobo/src/ext/treat_xrefs.rs
@@ -0,0 +1,495 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use crate::ast::*;
+
+use self::EntityFrame::*;
+
+/// Apply a single `treat-xrefs-as-equivalent` macro to the whole document.
+pub fn as_equivalent(
+    entities: &mut Vec<EntityFrame>,
+    prefix: &IdentPrefix
+) {
+    // Macro to reduce code duplication
+    macro_rules! process {
+        ($frame:ident, $clause:ident) => ({
+            let mut new = Vec::with_capacity($frame.clauses().len());
+            for clause in $frame.clauses() {
+                if let $clause::Xref(xref) = clause.as_ref() {
+                    if let Ident::Prefixed(p) = &xref.id {
+                        if &p.prefix == prefix {
+                            new.push(Line::from(
+                                $clause::EquivalentTo(xref.id.clone().into())
+                            ));
+                        }
+                    }
+                }
+            }
+            let clauses = $frame.clauses_mut();
+            for new_clause in new.into_iter() {
+                if !clauses.contains(&new_clause) {
+                    clauses.push(new_clause);
+                }
+            }
+        });
+    }
+
+    for entity in entities.iter_mut() {
+        match entity {
+            Term(x) => process!(x, TermClause),
+            Typedef(x) => process!(x, TypedefClause),
+            Instance(_) => (),
+        }
+    }
+}
+
+/// Apply a single `treat-xrefs-as-is_a` macro to the whole document.
+pub fn as_is_a(
+    entities: &mut Vec<EntityFrame>,
+    prefix: &IdentPrefix
+) {
+    // Macro to reduce code duplication
+    macro_rules! process {
+        ($frame:ident, $clause:ident) => ({
+            let mut new = Vec::with_capacity($frame.clauses().len());
+            for clause in $frame.clauses() {
+                if let $clause::Xref(xref) = clause.as_ref() {
+                    if let Ident::Prefixed(p) = &xref.id {
+                        if &p.prefix == prefix {
+                            new.push(Line::from(
+                                $clause::IsA(xref.id.clone().into())
+                            ));
+                        }
+                    }
+                }
+            }
+
+            let clauses = $frame.clauses_mut();
+            for new_clause in new.into_iter() {
+                if !clauses.contains(&new_clause) {
+                    clauses.push(new_clause);
+                }
+            }
+        });
+    }
+
+    for entity in entities.iter_mut() {
+        match entity {
+            Term(x) => process!(x, TermClause),
+            Typedef(x) => process!(x, TypedefClause),
+            Instance(_) => (),
+        }
+    }
+}
+
+/// Apply a single `treat-xrefs-as-is_a` macro to the whole document.
+pub fn as_has_subclass(
+    entities: &mut Vec<EntityFrame>,
+    prefix: &IdentPrefix
+) {
+    // Collect subclass info into a mapping where `key is_a value`
+    macro_rules! collect {
+        () => (HashMap::new());
+        ($frame:ident, $clause:ident) => ({
+            let mut new: HashMap<Ident, Ident> = HashMap::new();
+            for clause in $frame.clauses() {
+                if let $clause::Xref(xref) = clause.as_ref() {
+                    if let Ident::Prefixed(p) = &xref.id {
+                        if &p.prefix == prefix {
+                            new.insert(
+                                $frame.id().clone().into_inner().into(),
+                                xref.id.clone().into(),
+                            );
+                        }
+                    }
+                }
+            }
+            new
+        });
+    }
+
+    /// Add `is_a: $supercls` clause to the `$subcls` frame.
+    macro_rules! process {
+        ($subcls:ident, $supercls:ident, $clause:ident) => ({
+            $subcls.clauses_mut().push(
+                Line::from(
+                    $clause::IsA($supercls.clone().into())
+                )
+            );
+        })
+    }
+
+
+    // Collect a complete map of all `is_a` clauses that must be added.
+    let mut subclass_map: HashMap<Ident, HashSet<Ident>> = HashMap::new();
+    let mut entities_map: HashMap<Ident, &mut EntityFrame> = HashMap::new();
+    for entity in entities.iter_mut() {
+
+        let entity_mapping = match entity {
+            Term(x) => collect!(x, TermClause),
+            Typedef(x) => collect!(x, TypedefClause),
+            Instance(_) => collect!(),
+        };
+
+        for (key, value) in entity_mapping.into_iter() {
+            subclass_map.entry(key).or_default().insert(value);
+        }
+
+        entities_map.insert(entity.id().clone(), entity);
+    }
+
+    // Patch all entity frames with the xref id `is_a` clause.
+    for (superclass, subclasses) in subclass_map.into_iter() {
+        for subclass in subclasses.into_iter() {
+            match entities_map.get_mut(&subclass) {
+                Some(Term(ref mut x)) => process!(x, superclass, TermClause),
+                Some(Typedef(ref mut x)) => process!(x, superclass, TypedefClause),
+                _ => ()
+            }
+        }
+    }
+}
+
+/// Apply a single `treat-xrefs-as-genus-differentia` macro to the whole document.
+pub fn as_genus_differentia(
+    entities: &mut Vec<EntityFrame>,
+    prefix: &IdentPrefix,
+    relid: &RelationIdent,
+    classid: &ClassIdent,
+) {
+    for entity in entities.iter_mut() {
+        if let Term(x) = entity {
+            // Collect xrefs with the appropriate prefix
+            let mut has_intersection_of = false;
+            let mut new = Vec::with_capacity(x.clauses().len());
+            for clause in x.clauses() {
+                if let TermClause::Xref(xref) = clause.as_ref() {
+                    if let Ident::Prefixed(p) = &xref.id {
+                        if &p.prefix == prefix {
+                            // add genus from Xref
+                            new.push(Line::from(TermClause::IntersectionOf(
+                                None, xref.id.clone().into()
+                            )));
+                            // add differentia from header
+                            new.push(Line::from(TermClause::IntersectionOf(
+                                Some(relid.clone()), classid.clone()
+                            )));
+                        }
+                    }
+                } else if let TermClause::IntersectionOf(_, _) = clause.as_ref() {
+                    has_intersection_of = true;
+                }
+            }
+            // Apply the genus-differentia clause
+            // if the frame has no `intersection_of`
+            if !has_intersection_of {
+                let clauses = x.clauses_mut();
+                for new_clause in new.into_iter() {
+                    if !clauses.contains(&new_clause) {
+                        clauses.push(new_clause);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/// Apply a single `treat-xrefs-as-reverse-genus-differentia` macro to the whole document.
+pub fn as_reverse_genus_differentia(
+    entities: &mut Vec<EntityFrame>,
+    prefix: &IdentPrefix,
+    relid: &RelationIdent,
+    classid: &ClassIdent
+) {
+    /// Collect genus info into a mapping where `value := intersection_of key`
+    macro_rules! collect {
+        () => (HashMap::new());
+        ($frame:ident, $clause:ident) => ({
+            let mut new: HashMap<Ident, Ident> = HashMap::new();
+            for clause in $frame.clauses() {
+                if let $clause::Xref(xref) = clause.as_ref() {
+                    if let Ident::Prefixed(p) = &xref.id {
+                        if &p.prefix == prefix {
+                            new.insert(
+                                $frame.id().clone().into_inner().into(),
+                                xref.id.clone().into(),
+                            );
+                        }
+                    }
+                }
+            }
+            new
+        })
+    }
+
+    /// Add genus-differentia to the other frame.
+    macro_rules! process {
+        ($frame:ident, $genus:ident, $clause:ident) => ({
+            let clauses = $frame.clauses_mut();
+            clauses.push(
+                Line::from(
+                    $clause::IntersectionOf(None, $genus.clone().into())
+                )
+            );
+            clauses.push(
+                Line::from(
+                    $clause::IntersectionOf(Some(relid.clone()), classid.clone())
+                )
+            );
+        })
+    }
+
+    // Collect a complete map of all `is_a` clauses that must be added.
+    let mut subclass_map: HashMap<Ident, HashSet<Ident>> = HashMap::new();
+    let mut entities_map: HashMap<Ident, &mut EntityFrame> = HashMap::new();
+    for entity in entities.iter_mut() {
+
+        let entity_mapping = match entity {
+            Term(x) => collect!(x, TermClause),
+            Typedef(_) => collect!(),
+            Instance(_) => collect!(),
+        };
+
+        for (key, value) in entity_mapping.into_iter() {
+            subclass_map.entry(key).or_default().insert(value);
+        }
+
+        entities_map.insert(entity.id().clone(), entity);
+    }
+
+    // Patch all entity frames with the right `intersection_of` clauses.
+    for (genus, classes) in subclass_map.into_iter() {
+        for cls in classes.into_iter() {
+            if let Some(Term(ref mut x)) = entities_map.get_mut(&cls) {
+                process!(x, genus, TermClause)
+            }
+        }
+    }
+}
+
+/// Apply a single `treat-xrefs-as-relationship` macro to the whole document.
+pub fn as_relationship(
+    entities: &mut Vec<EntityFrame>,
+    prefix: &IdentPrefix,
+    relid: &RelationIdent
+) {
+    // Macro to reduce code duplication
+    macro_rules! process {
+        ($frame:ident, $clause:ident) => ({
+            let mut new = Vec::with_capacity($frame.clauses().len());
+            for clause in $frame.clauses() {
+                if let $clause::Xref(xref) = clause.as_ref() {
+                    if let Ident::Prefixed(p) = &xref.id {
+                        if &p.prefix == prefix {
+                            new.push(Line::from(
+                                $clause::Relationship(
+                                    relid.clone(),
+                                    xref.id.clone().into()
+                                )
+                            ));
+                        }
+                    }
+                }
+            }
+
+            let clauses = $frame.clauses_mut();
+            for new_clause in new.into_iter() {
+                if !clauses.contains(&new_clause) {
+                    clauses.push(new_clause);
+                }
+            }
+        });
+    }
+
+    for entity in entities.iter_mut() {
+        match entity {
+            Term(x) => process!(x, TermClause),
+            Typedef(x) => process!(x, TypedefClause),
+            Instance(x) => process!(x, InstanceClause),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::str::FromStr;
+    use std::string::ToString;
+
+    use pretty_assertions::assert_eq;
+    use textwrap::dedent;
+
+    use super::*;
+    use crate::ext::OboSemantics;
+
+    #[test]
+    fn as_equivalent() {
+        let mut doc = OboDoc::from_str(&dedent("
+            treat-xrefs-as-equivalent: TEST
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+        ")).unwrap();
+
+        doc.treat_xrefs();
+
+        self::assert_eq!(dedent("
+            treat-xrefs-as-equivalent: TEST
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+            equivalent_to: TEST:002
+
+            [Term]
+            id: TEST:002
+        ").trim_start_matches('\n'), doc.to_string());
+    }
+
+    #[test]
+    fn as_is_a() {
+        let mut doc = OboDoc::from_str(&dedent("
+            treat-xrefs-as-is_a: TEST
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+        ")).unwrap();
+
+        doc.treat_xrefs();
+
+        self::assert_eq!(dedent("
+            treat-xrefs-as-is_a: TEST
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+            is_a: TEST:002
+
+            [Term]
+            id: TEST:002
+        ").trim_start_matches('\n'), doc.to_string());
+    }
+
+    #[test]
+    fn as_has_subclass() {
+        let mut doc = OboDoc::from_str(&dedent("
+            treat-xrefs-as-has-subclass: TEST
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+        ")).unwrap();
+
+        doc.treat_xrefs();
+
+        self::assert_eq!(dedent("
+            treat-xrefs-as-has-subclass: TEST
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+            is_a: TEST:001
+        ").trim_start_matches('\n'), doc.to_string());
+    }
+
+    #[test]
+    fn as_genus_differentia() {
+        let mut doc = OboDoc::from_str(&dedent("
+            treat-xrefs-as-genus-differentia: TEST part_of something
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+        ")).unwrap();
+
+        doc.treat_xrefs();
+
+        self::assert_eq!(dedent("
+            treat-xrefs-as-genus-differentia: TEST part_of something
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+            intersection_of: TEST:002
+            intersection_of: part_of something
+
+            [Term]
+            id: TEST:002
+        ").trim_start_matches('\n'), doc.to_string());
+    }
+
+    #[test]
+    fn as_reverse_genus_differentia() {
+        let mut doc = OboDoc::from_str(&dedent("
+            treat-xrefs-as-reverse-genus-differentia: TEST part_of something
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+        ")).unwrap();
+
+        doc.treat_xrefs();
+
+        self::assert_eq!(dedent("
+            treat-xrefs-as-reverse-genus-differentia: TEST part_of something
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+            intersection_of: TEST:001
+            intersection_of: part_of something
+        ").trim_start_matches('\n'), doc.to_string());
+    }
+
+    #[test]
+    fn as_relationship() {
+        let mut doc = OboDoc::from_str(&dedent("
+            treat-xrefs-as-relationship: TEST connected_to
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+
+            [Term]
+            id: TEST:002
+        ")).unwrap();
+
+        doc.treat_xrefs();
+
+        self::assert_eq!(dedent("
+            treat-xrefs-as-relationship: TEST connected_to
+
+            [Term]
+            id: TEST:001
+            xref: TEST:002
+            relationship: connected_to TEST:002
+
+            [Term]
+            id: TEST:002
+        ").trim_start_matches('\n'), doc.to_string());
+    }
+}

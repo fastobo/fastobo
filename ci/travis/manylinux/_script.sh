07f28b7fe8f1009b1af312cbc40ad92d1133ebdd
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-05-06 12:13:49 -0700

    Fix timeouts in Travis-CI caused by missing `requests` in manylinux Docker containers

--- ci/travis/manylinux/_script.sh
+++ ci/travis/manylinux/_script.sh
@@ -7,7 +7,7 @@ export PYTHON_LIB=$(${PYBIN}/python -c "import sysconfig; print(sysconfig.get_co
 export LIBRARY_PATH="$LIBRARY_PATH:$PYTHON_LIB"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PYTHON_LIB"
 
-"${PYBIN}/pip" install -U setuptools setuptools-rust
+"${PYBIN}/pip" install -U setuptools setuptools-rust requests
 
 cd /io
 $PYTHON_SYS_EXECUTABLE setup.py test

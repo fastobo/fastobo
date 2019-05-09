3c3003dcfbc509d869741bb0d2b8c823d424888e
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-05-08 21:53:15 -0700

    Add test framework to check canonical version of OBO documents

---
+++ fastobo/tests/canonicalize.rs
@@ -0,0 +1,41 @@
+extern crate pretty_assertions;
+
+extern crate fastobo;
+
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use pretty_assertions::assert_eq;
+
+use fastobo::ast::OboDoc;
+
+
+macro_rules! canonicalizetest {
+    ($name:ident) => {
+        #[test]
+        fn $name() {
+
+            let dir = {
+                let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+                p.push("tests");
+                p.push("data");
+                p.push("canonicalize");
+                p
+            };
+
+            let input_path = dir.join(format!("{}.input.obo", stringify!($name)));
+            let output_path = dir.join(format!("{}.output.obo", stringify!($name)));
+
+            let mut doc = OboDoc::from_file(&input_path).unwrap();
+            doc.header_mut().sort_unstable();
+
+            println!("{}", doc.header());
+
+            let output = std::fs::read_to_string(&output_path).unwrap();
+            assert_eq!(doc.to_string(), output);
+        }
+    }
+}
+
+
+canonicalizetest!(header);

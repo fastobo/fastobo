d8cb7987a8525f56a45794193584d9261184dafa
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-07-24 02:23:14 +0000

    Setup `cargo-cache` to reduce cache size in Travis-CI

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -14,3 +14,15 @@ if [ "$LATEST" != "$LOCAL" ]; then
 else
 	log Using cached cargo-tarpaulin v$LOCAL
 fi
+
+# --- Setup cargo-cache ------------------------------------------------------
+
+LATEST=$(cargo search cargo-cache | head -n1 | cut -f2 -d"\"")
+LOCAL=$(cargo cache --version 2>/dev/null | cut -d" " -f2 || echo "none")
+
+if [ "$LATEST" != "$LOCAL" ]; then
+        log Installing cargo-cache v$LATEST
+        cargo install -f cargo-cache --root "$HOME/.cargo"
+else
+        log Using cached cargo-cache v$LOCAL
+fi

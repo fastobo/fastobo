c875478883fe9357768a435841626e43dfaa7365
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-10-05 16:41:10 +0200

    Attempt fixing `chandler` issues in Travis-CI

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -22,7 +22,7 @@ export GEM_PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')"
 export PATH="${GEM_PATH}/bin:$PATH"
 
 log Installing chandler gem
-gem install --user-install chandler
+gem install --user-install 'faraday:<0.16' chandler
 
 log Updating GitHub release notes
 chandler push --github="$TRAVIS_REPO_SLUG" --changelog="CHANGELOG.md"

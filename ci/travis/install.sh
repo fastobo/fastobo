4407c66ffe6c5f0e432e2cc5c0d373fde4423f66
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-07 12:56:20 +0100

    Compile `tarpaulin` from source in Travis-CI

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -8,9 +8,8 @@ LATEST=$(cargo search cargo-tarpaulin | grep cargo-tarpaulin | cut -f2 -d"\"")
 LOCAL=$(cargo tarpaulin --version 2>/dev/null | cut -d" " -f3 || echo "none")
 
 if [ "$LATEST" != "$LOCAL" ]; then
-	log Downloading cargo-tarpaulin v$LATEST
-	URL="https://github.com/xd009642/tarpaulin/releases/download/${LATEST}/cargo-tarpaulin-${LATEST}-travis.tar.gz"
-	curl -SsL "$URL" | tar xzC "$HOME/.cargo/bin"
+	log Installing cargo-tarpaulin v$LATEST
+	cargo install -f cargo-tarpaulin --root "$HOME/.cargo"
 else
 	log Using cached cargo-tarpaulin v$LOCAL
 fi

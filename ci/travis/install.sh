79d49d2b8c439ae9039ec41ba74ce388bad2f5e8
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-04-10 13:32:23 +0200

    Streamline management of `cargo` build tools in CI

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -1,27 +1,17 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
 
 . $(dirname $0)/functions.sh
 
-# --- Setup cargo-tarpaulin --------------------------------------------------
+# --- Setup cargo-tarpaulin ----------------------------------------------------------
 
 LATEST=$(cargo search cargo-tarpaulin | grep cargo-tarpaulin | cut -f2 -d"\"")
-LOCAL=$(cargo tarpaulin --version 2>/dev/null | cut -d" " -f3 || echo "none")
-
-if [ "$LATEST" != "$LOCAL" ]; then
-	log Installing cargo-tarpaulin v$LATEST
-	cargo install -f cargo-tarpaulin --root "$HOME/.cargo"
-else
-	log Using cached cargo-tarpaulin v$LOCAL
-fi
+log Downloading cargo-tarpaulin v$LATEST
+URL="https://github.com/xd009642/tarpaulin/releases/download/${LATEST}/cargo-tarpaulin-${LATEST}-travis.tar.gz"
+curl -SsL "$URL" | tar xzvC "$HOME/.cargo/bin"
 
 # --- Setup cargo-cache ------------------------------------------------------
 
-LATEST=$(cargo search cargo-cache | head -n1 | cut -f2 -d"\"")
-LOCAL=$(cargo cache --version 2>/dev/null | cut -d" " -f2 || echo "none")
-
-if [ "$LATEST" != "$LOCAL" ]; then
-        log Installing cargo-cache v$LATEST
-        cargo install -f cargo-cache --root "$HOME/.cargo"
-else
-        log Using cached cargo-cache v$LOCAL
-fi
+log Installing latest cargo-cache
+cargo install -f cargo-cache --no-default-features --features ci-autoclean --root "$HOME/.cargo"
--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -5,11 +5,11 @@
 # --- Test without features --------------------------------------------------
 
 log Testing without default features
-cargo test --release --no-default-features
+cargo tarpaulin --release -v --out Xml --ciserver travis-ci --no-default-features
 
 # --- Test with coverage -----------------------------------------------------
 
-log Measuring code coverage through Tarpaulin with default features
+log Testing with default features
 cargo tarpaulin --release -v --out Xml --ciserver travis-ci
 
 # --- Run examples -----------------------------------------------------------

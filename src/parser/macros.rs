b391b4bcd522cb4a5a9da042866eb4f00f18fb9d
Martin Larralde <martin.larralde@embl.de>
2020-06-14 17:27:08 +0200

    Remove unused `impl_fromslice` macro in `fastobo::parser::macros`

--- src/parser/macros.rs
+++ src/parser/macros.rs
@@ -42,37 +42,3 @@ macro_rules! impl_fromstr {
         }
     };
 }
-
-// FIXME: Proper type !
-/// A macro to provide `FromSlice` implementation to `FromPair` implementors.
-macro_rules! impl_fromslice {
-    ($life:lifetime, $type:ty) => {
-        impl<$life> $crate::parser::FromSlice<$life> for $type {
-            type Err = $crate::error::SyntaxError;
-            fn from_slice(s: &$life str) -> Result<Self, Self::Err> {
-                use $crate::error::Error;
-                use $crate::parser::OboParser;
-                use $crate::pest::error::ErrorVariant;
-                use $crate::pest::Parser;
-                use $crate::pest::Position;
-
-                // Parse the input string
-                let mut pairs = OboParser::parse(Self::RULE, s)?;
-                let pair = pairs.next().unwrap();
-                // Check EOI was reached
-                if pair.as_span().end() != s.len() {
-                    let span = pair
-                        .as_span()
-                        .end_pos()
-                        .span(&Position::new(s, s.len()).unwrap());
-                    let variant = ErrorVariant::CustomError {
-                        message: "remaining input".to_string(),
-                    };
-                    Err($crate::pest::error::Error::new_from_span(variant, span).into())
-                } else {
-                    unsafe { <Self as FromPair>::from_pair_unchecked(pair) }
-                }
-            }
-        }
-    };
-}
